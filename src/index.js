b560aef7236e1e09acd760ff08c132a59984ccbc
dnyaneshwargit <dnyaneshwar1231@gmail.com>
2020-03-17 18:17:24 +0530

    Create gh-pages branch with static content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './responsive.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Suit from './components/Suit/Suit';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+
+ReactDOM.render(<Suit />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
